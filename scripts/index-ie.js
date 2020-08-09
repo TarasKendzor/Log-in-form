a1414e8e0a2769096d5e4225d20ab0df3ebe0f0a
Taras  Kendzor <45481754+TarasKendzor@users.noreply.github.com>
2020-08-09 17:30:46 +0300

    code review

--- scripts/index-ie.js
+++ scripts/index-ie.js
@@ -1,3 +1,4 @@
+//   Array.from method polyfill start . //
 if (!Array.from) {
     Array.from = (function () {
         var toStr = Object.prototype.toString;
@@ -16,48 +17,30 @@ if (!Array.from) {
             return Math.min(Math.max(len, 0), maxSafeInteger);
         };
 
-        // Свойство length метода from равно 1.
         return function from(arrayLike/*, mapFn, thisArg */) {
-            // 1. Положим C равным значению this.
             var C = this;
 
-            // 2. Положим items равным ToObject(arrayLike).
             var items = Object(arrayLike);
 
-            // 3. ReturnIfAbrupt(items).
             if (arrayLike == null) {
                 throw new TypeError('Array.from requires an array-like object - not null or undefined');
             }
-
-            // 4. Если mapfn равен undefined, положим mapping равным false.
             var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
             var T;
             if (typeof mapFn !== 'undefined') {
-                // 5. иначе
-                // 5. a. Если вызов IsCallable(mapfn) равен false, выкидываем исключение TypeError.
+
                 if (!isCallable(mapFn)) {
                     throw new TypeError('Array.from: when provided, the second argument must be a function');
                 }
-
-                // 5. b. Если thisArg присутствует, положим T равным thisArg; иначе положим T равным undefined.
                 if (arguments.length > 2) {
                     T = arguments[2];
                 }
             }
 
-            // 10. Положим lenValue равным Get(items, "length").
-            // 11. Положим len равным ToLength(lenValue).
             var len = toLength(items.length);
 
-            // 13. Если IsConstructor(C) равен true, то
-            // 13. a. Положим A равным результату вызова внутреннего метода [[Construct]]
-            //     объекта C со списком аргументов, содержащим единственный элемент len.
-            // 14. a. Иначе, положим A равным ArrayCreate(len).
             var A = isCallable(C) ? Object(new C(len)) : new Array(len);
-
-            // 16. Положим k равным 0.
             var k = 0;
-            // 17. Пока k < len, будем повторять... (шаги с a по h)
             var kValue;
             while (k < len) {
                 kValue = items[k];
@@ -68,13 +51,14 @@ if (!Array.from) {
                 }
                 k += 1;
             }
-            // 18. Положим putStatus равным Put(A, "length", len, true).
             A.length = len;
-            // 20. Вернём A.
             return A;
         };
     }());
 }
+
+//   Array.from method polyfill end . //
+
 //  Input placeholder logic  start. //
 var placeholder = document.querySelectorAll('.placeholder');
 var input = document.querySelectorAll('.inputs');
